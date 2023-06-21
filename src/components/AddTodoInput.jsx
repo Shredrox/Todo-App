f589142ef45f826e83067e07e4ce6c8a60d2a770
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-21 20:48:45 +0300

    Added empty string check

--- src/components/AddTodoInput.jsx
+++ src/components/AddTodoInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const AddTodoInput = ({addTodo, setTodoContent, todoContent}) => {
   
   const handleKeyDown = (e) =>{
-    if(e.key === 'Enter'){
+    if(e.key === 'Enter' && todoContent !== ''){
       addTodo();
     }
   }  
