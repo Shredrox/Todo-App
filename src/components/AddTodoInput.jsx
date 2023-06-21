4d9905650a8853f7274998feff7d7cecc1a16d8a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-21 20:46:34 +0300

    Added keyDown handle

--- src/components/AddTodoInput.jsx
+++ src/components/AddTodoInput.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export const AddTodoInput = ({addTodo, setTodoContent, todoContent}) => {
-    
-    return (
-      <div className="add-todo-container">
-          <input onChange={(e) => setTodoContent(e.target.value)} value={todoContent} type="text" placeholder='...'/>
-          <button onClick={addTodo} className='add-button' disabled={todoContent === '' ? true : false}>Add Todo</button>
-      </div>
-    )
+  
+  const handleKeyDown = (e) =>{
+    if(e.key === 'Enter'){
+      addTodo();
+    }
+  }  
+
+  return (
+    <div className="add-todo-container">
+        <input onKeyDown={handleKeyDown} onChange={(e) => setTodoContent(e.target.value)} value={todoContent} type="text" placeholder='...'/>
+        <button onClick={addTodo} className='add-button' disabled={todoContent === '' ? true : false}>Add Todo</button>
+    </div>
+  )
 }
