4f08e6bb4434c0974588f7a7aec5fb68403d999d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-21 15:40:25 +0300

    Added TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TodoItem } from './TodoItem'
+
+export const TodoList = () => {
+  return (
+    <div className='todo-list'>
+        <TodoItem/>
+    </div>
+  )
+}
