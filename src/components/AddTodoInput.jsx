f5fa74c799b9ab9619576faee665e7d664c144d8
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-21 19:11:24 +0300

    Adjusted AddTodoInput

--- src/components/AddTodoInput.jsx
+++ src/components/AddTodoInput.jsx
@@ -4,8 +4,8 @@ export const AddTodoInput = ({addTodo, setTodoContent, todoContent}) => {
     
     return (
       <div className="add-todo-container">
-          <input onChange={(e) => setTodoContent(e.target.value)} value={todoContent} type="text" />
-          <button onClick={addTodo} className='add-button'>Add Todo</button>
+          <input onChange={(e) => setTodoContent(e.target.value)} value={todoContent} type="text" placeholder='...'/>
+          <button onClick={addTodo} className='add-button' disabled={todoContent === '' ? true : false}>Add Todo</button>
       </div>
     )
 }
